12be5bdf0e3e86e430078e7831db98c00f66fc17
CreamCrusher <nicita.zolotov@gmail.com>
2025-07-18 17:12:01 +0300

    feat: update tests

--- Services/TemplateApiServise/Test/User/test_user_queries.py
+++ Services/TemplateApiServise/Test/User/test_user_queries.py
@@ -1,22 +1,9 @@
 import pytest
 
-from Services.TemplateApiServise.Application.Users.user_dtos import GetUserByIdDTO
 from Services.TemplateApiServise.Application.exceptions.ModelNotFound import ModelNotFound
 from Services.TemplateApiServise.WebApi.Controllers.UserController import get_user_by_id_api
 
 pytest_plugins = ('pytest_asyncio',)
-
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_get_user_by_id():
-    # Arrange
-    user_id = 1
-
-    # Act
-    user = await get_user_by_id_api(user_id)
-
-    # Assert
-    assert isinstance(user, GetUserByIdDTO)
 
 
 @pytest.mark.asyncio(loop_scope="session")
