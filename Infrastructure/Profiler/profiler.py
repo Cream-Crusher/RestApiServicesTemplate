ba7ff47b0ee9fcbe26d4186b12542ae0fc03df35
CreamCrusher <nicita.zolotov@gmail.com>
2025-08-08 19:55:50 +0300

    feat: update profile and gitignore

--- Infrastructure/Profiler/profiler.py
+++ Infrastructure/Profiler/profiler.py
@@ -7,7 +7,6 @@ from line_profiler import LineProfiler
 from config import settings
 
 FULL_PATH = pathlib.Path(__file__).parent.resolve()
-line_profiler = LineProfiler()
 
 
 def profiler[F](prefixname: str = "") -> Callable[[F], F]:
@@ -15,6 +14,8 @@ def profiler[F](prefixname: str = "") -> Callable[[F], F]:
         async def wrapper(*args: Any, **kwargs: Any) -> Any:
             if settings.app_config.environment_type != "local":
                 return await func(*args, **kwargs)
+
+            line_profiler = LineProfiler()
 
             line_profiler.add_function(func)
             line_profiler.enable()
@@ -28,6 +29,8 @@ def profiler[F](prefixname: str = "") -> Callable[[F], F]:
 
                 with open(f"{FULL_PATH}/{prefixname}{func.__name__}_profiler_result.txt", "a") as f:  # NOSONAR
                     line_profiler.print_stats(stream=f)
+                    f.seek(0)
+                    f.close()
 
         return cast(F, wrapper)
 
